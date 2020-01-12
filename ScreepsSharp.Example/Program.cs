fb99853bfd0437cf5c76214493f8ea6af9115534
puntastic <xxobot@gmail.com>
2020-01-13 03:47:39 +1000

    Fix for null reference exception.

--- ScreepsSharp.Example/Program.cs
+++ ScreepsSharp.Example/Program.cs
@@ -22,30 +22,30 @@ namespace ScreepsSharp.Example
             var js = Game.js;
 
             Log("Main Called, it wants its javascript back.");
-            // js.InvokeVoid("console.log", "MainCalled");
-            var spawnNames = js.Invoke<string[]>("bindings.spawnNames");
+            //// js.InvokeVoid("console.log", "MainCalled");
+            //var spawnNames = js.Invoke<string[]>("bindings.spawnNames");
 
-            foreach (var spawnName in spawnNames)
-            {
-                Log(spawnName);
-                var creep = Game.creeps[spawnName];
-                if (creep == null)
-                {
-                    _ = js.Invoke<int>($"Game.spawns.{spawnName}.createCreep", new string[] { "move", "carry", "work" }, spawnName);
-                    continue;
-                }
+            //foreach (var spawnName in spawnNames)
+            //{
+            //    Log(spawnName);
+            //    var creep = Game.creeps[spawnName];
+            //    if (creep == null)
+            //    {
+            //        _ = js.Invoke<int>($"Game.spawns.{spawnName}.createCreep", new string[] { "move", "carry", "work" }, spawnName);
+            //        continue;
+            //    }
 
-                var roomRef = $"Game.spawns.{spawnName}.room";
+            //    var roomRef = $"Game.spawns.{spawnName}.room";
 
-                if (js.Invoke<int>($"Game.creeps.{creep.name}.store.energy") == 0)
-                {
-                    creep.MoveTo(js.Invoke<RoomPosition>($"Game.spawns.{spawnName}.pos"));
-                }
-                // if(js.Invoke<int>("Game.creeps.carry.energy") == 0)
-                //{
-                js.Invoke<int>($"Game.creeps.{creep.name}.say", "I can C#ly", true);
-                //}
-            }
+            //    if (js.Invoke<int>($"Game.creeps.{creep.name}.store.energy") == 0)
+            //    {
+            //        creep.MoveTo(js.Invoke<RoomPosition>($"Game.spawns.{spawnName}.pos"));
+            //    }
+            //    // if(js.Invoke<int>("Game.creeps.carry.energy") == 0)
+            //    //{
+            //    js.Invoke<int>($"Game.creeps.{creep.name}.say", "I can C#ly", true);
+            //    //}
+            //}
             //    CreateHostBuilder(args).Build().Run();
         }
     }
