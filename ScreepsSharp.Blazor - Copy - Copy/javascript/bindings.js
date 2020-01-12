c6d2306f66b779a64e9a62035565c8b744709fd9
puntastic <xxobot@gmail.com>
2020-01-13 03:31:27 +1000

    Remove duplicate project.

--- ScreepsSharp.Blazor - Copy - Copy/javascript/bindings.js
+++
@@ -1,6 +0,0 @@
-global.bindings =
-{
-	roomNames: function() { return Object.keys(Game.rooms); },
-	creepNames: function() { return Object.keys(Game.creeps); },
-	spawnNames: function() { return Object.keys(Game.spawns); }
-}
